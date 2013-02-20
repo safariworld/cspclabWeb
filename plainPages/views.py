217715af623c74e9c6912e087a6de1f48106d481
bosung <bosung.kim3416@gmail.com>
2013-02-20 16:19:32 +0900

    sign up is added.

--- plainPages/views.py
+++ plainPages/views.py
@@ -7,25 +7,28 @@ from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render_to_response
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
 from cspclabWeb.forms import *
 
 def aboutUs(request):
-    template = get_template('aboutUs.html')	
-    var = Context({
-        'head_title': 'About Us',
-    })
-    return HttpResponse( template.render(var) )
-
+    var = RequestContext(request,{
+        'head_title':'About Us',})
+    
+    return render_to_response(
+            'aboutUs.html',
+            var,
+    )
+   
 def register(request):
-    if request.method == 'post':
+    if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = User.objects.create_user(
                     username=form.cleaned_data['username'],
                     password=form.cleaned_data['password1'],
                     email=form.cleaned_data['email']
-                    )
-            return HttpResponseRedirect('/accounts/register_success/')
+            )
+            return HttpResponseRedirect('/plain/accounts/register_success/')
     else:
         form = RegistrationForm()
 
@@ -38,22 +41,34 @@ def register(request):
     )
 
 def achievements(request):
-    template = get_template('achievements.html')
-    var = Context({
+    
+    var = RequestContext(request,{
+
     })
-    return HttpResponse( template.render(var) )
+    return render_to_response(
+            'achievements.html',
+            var,
+    )
 
 def calendar(request):
-    template = get_template('calendar.html')
-    var = Context({
+    
+    var = RequestContext(request,{
+
         })
-    return HttpResponse( template.render(var) )
+    return render_to_response(
+            'calendar.html',
+            var,
+    )
 
 def profile(request):
-    template = get_template('profile.html')
-    var = Context({
+
+    var = RequestContext(request,{
+
         })
-    return HttpResponse( template.render(var) )
+    return render_to_response(
+            'profile.html',
+            var
+            )
 
 def logout_page(request):
     logout(request)
